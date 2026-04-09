ca0904f14b091791399683c4695edc3c75ca49e1
taida8152-boop <taida8152@gmail.com>
2026-04-09 03:35:09 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import { componentTagger } from "lovable-tagger";
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   server: {'/prism-pulse-market/',
-    host: "::",
+    host: "",
     port: 8080,
     hmr: {
       overlay: false,
